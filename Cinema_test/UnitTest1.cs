af88343c016b2b01978755e350e8c60f06c043ed
StanTop <saj.tophoven@student.avans.nl>
2024-02-05 13:39:58 +0100

    more tests

--- Cinema_test/UnitTest1.cs
+++ Cinema_test/UnitTest1.cs
@@ -2,6 +2,7 @@ namespace Cinema_test
 {
     public class UnitTest1
     {
+        //1,2,4
         [Fact]
         public void IsAStudentAndHasSecondTicketThatIsNotPremumShouldEqual10()
         {
@@ -18,7 +19,48 @@ namespace Cinema_test
 
 
             Assert.Equal(10, result);
-
         }
+        //1,2,5,6
+        [Fact]
+        public void IsAStudentAndIsNotSecondTicketThatIsPremiumShouldEqual10()
+        {
+            Movie movie = new Movie("The matrix");
+            MovieScreening movieScreening = new(movie, DateTime.Now, 10);
+            MovieTicket movieTicket1 = new(movieScreening, 1, 2, false);
+            Order order = new(1, true);
+            order.addSeatReservation(movieTicket1);
+            decimal result = order.calculatePrice();
+            Assert.Equal(10, result);
+        }
+        //1,2,5,7
+        [Fact]
+        public void IsAStudentAndIsNotSecondTicketThatIsPremiumShouldEqual12()
+        {
+            Movie movie = new Movie("The matrix");
+            MovieScreening movieScreening = new(movie, DateTime.Now, 10);
+            MovieTicket movieTicket1 = new(movieScreening, 1, 2, true);
+            Order order = new(1, true);
+            order.addSeatReservation(movieTicket1);
+            decimal result = order.calculatePrice();
+            Assert.Equal(12, result);
+        }
+
+        //1,3,8,11,13
+        [Fact]
+        public void IsNotAStudentAndWithinTheWeekendThatIsPremiumAndGroupIsNotBiggerThan6ShouldEqual()
+        {
+
+            DateTime monday = new DateTime(2024, 2, 5);
+            Movie movie = new("The matrix");
+            MovieScreening movieScreening = new(movie, monday, 10);
+            MovieTicket movieTicket1 = new(movieScreening, 1, 2, true);
+            Order order = new(1, false);
+            order.addSeatReservation(movieTicket1);
+            decimal result = order.calculatePrice();
+            Assert.Equal(13, result);
+        }
+
     }
+
+    #if(true && (false || false))
 }
